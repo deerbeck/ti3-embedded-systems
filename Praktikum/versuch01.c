83b82379b79f3bbde73e8cb038116ff7127459cf
Prof. Dr. Benjamin Kormann <benjamin.kormann@hm.edu>
2023-04-02 11:34:24 +0200

    Aufgabenstellung zum ersten Praktikumsversuch hinzugefügt

---
+++ Praktikum/versuch01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "ti_bmp.h"
+
+
+// main application
+int main()
+{
+//    int32_t width = 200;
+//    int32_t height = 100;
+//    uint32_t datasize = height * width;
+//    uint8_t image[datasize];
+
+//    for (uint32_t i = 0; i < datasize; i++)
+//    {
+//        image[i] = 234;
+//   }
+
+//    saveBmpGray("hello.bmp", width, height, image);
+
+    // end application successfully
+    return 0;
+}
