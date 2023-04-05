28eaefa332e8bbd312cd470c825543f8e565e51d
johanneshi <johannes.hirschbeck@hm.edu>
2023-04-05 16:37:19 +0200

    tested c coding

--- Uebungsaufgaben/K2-Verschluesselung/K2-Verschluesselung/main.c
+++ Uebungsaufgaben/K2-Verschluesselung/K2-Verschluesselung/main.c
@@ -6,25 +6,26 @@ char* encoding();
 int main()
 {
     char text[] = "Test";
-    char key[] = "Hallo";
-    encoding(text, key);
+    int s_len = strlen(text);
+    char result[s_len +1];
+
+    char key[] = "Hall";
+    printf("s\n",encoding(text, key, result));
+    printf("s\n",encoding(text, key, result));
+
+    //printf("%c\n", encoding(en_text, key));
     return 0;
 }
 
 
-char* encoding(char* text, char* key)
+char* encoding(char* text, char* key, char* result)
 {
-    int s_len = strlen(text), test1 = 0, i = 0;
-    char result[s_len + 1];
+    int s_len = strlen(text);
 
-    while(*(text+i) != '\0')
+    for(int i = 0; i < s_len; i++)
     {
-        i++;
         *(result + i) = (*(text+i))^((0b00001111)&*(key+(i%s_len)));
-
-        //("%b\n", *(text));
-        //printf("\n");
-        printf("%d\n",i);
-        //printf("%c\n", *(key+i));
+        //*(result + i) = (*(text+i));
     }
+    *(result+ (s_len +1)) = '\0';
 }
