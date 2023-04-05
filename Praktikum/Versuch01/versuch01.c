fe49786b2a246dddc67cbc4171d4b630eb8d94cf
johanneshi <johannes.hirschbeck@hm.edu>
2023-04-05 19:57:51 +0200

    completed task01 from assignment 1 and created first bitmap picture

--- Praktikum/Versuch01/versuch01.c
+++ Praktikum/Versuch01/versuch01.c
@@ -5,18 +5,17 @@
 // main application
 int main()
 {
-//    int32_t width = 200;
-//    int32_t height = 100;
-//    uint32_t datasize = height * width;
-//    uint8_t image[datasize];
+    int32_t width = 200;
+    int32_t height = 100;
+    uint32_t datasize = height * width;
+    uint8_t image[datasize];
+    for (uint32_t i = 0; i < datasize; i++)
+    {
+        image[i] = 234;
+   }
 
-//    for (uint32_t i = 0; i < datasize; i++)
-//    {
-//        image[i] = 234;
-//   }
+    saveBmpGray("hello.bmp", width, height, image);
 
-//    saveBmpGray("hello.bmp", width, height, image);
-
-    // end application successfully
+// end application successfully
     return 0;
 }
