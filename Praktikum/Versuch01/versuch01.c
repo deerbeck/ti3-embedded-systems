8be2216a15518d87903877316ab9ff2fbbdec9bd
johanneshi <johannes.hirschbeck@hm.edu>
2023-04-25 11:50:08 +0200

    submitted assignment01 in class

--- Praktikum/Versuch01/versuch01.c
+++ Praktikum/Versuch01/versuch01.c
@@ -24,41 +24,46 @@ int main()
     //buffer for input
     char buffer[255];
 
-    while(1){
-    printf("Geben Sie den Basisdateinamen , die Bildbreite und die Anzahl der Kreise ein (getrennt durch Leerzeichen): \n");
-    //Read Input from keyboard and print spacers to make consoleoutput more readable
-    fgets(buffer, sizeof(buffer), stdin);
-    printf("\n");
+    while(1)
+    {
+        printf("Geben Sie den Basisdateinamen, die Bildbreite und die Anzahl der Kreise ein (getrennt durch Leerzeichen): \n");
+        //Read Input from keyboard and print spacers to make consoleoutput more readable
+        fgets(buffer, sizeof(buffer), stdin);
+        printf("\n");
 
-    // use sscanf() to extract integers from input line and check for correct input (just use adress from width and circleCount for more readability)
-    if (sscanf(buffer, "%s %d %hhu",name, &width, &circleCount) == 3) {
-        //check for negative input and end program
-        if(width < 0 || circleCount < 0){
-        printf("Fehlerhafte Eingabe, Programm wird beendet!\n\n");
-        break;
+        // use sscanf() to extract integers from input line and check for correct input (just use adress from width and circleCount for more readability)
+        if (sscanf(buffer, "%s %d %hhu",name, &width, &circleCount) == 3)
+        {
+            //check for negative input and end program
+            if(width < 0 || circleCount < 0)
+            {
+                printf("Fehlerhafte Eingabe, Programm wird beendet!\n\n");
+                break;
+            }
+
+            //print out input to give user feedback
+            printf("Bild wird mit folgenden Parametern erzeugt:\n   -Basisdateiname: %s\n   -Bildbreite: %d\n   -Anzahl der Kreise: %d\n.\n.\n.\n", name, width, circleCount);
+        }
+        else
+        {
+            printf("Fehlerhafte Eingabe, Programm wird beendet!\n\n");
+            break;
         }
 
-        //print out input to give user feedback
-        printf("Bild wird mit folgenden Parametern erzeugt:\n   -Basisdateiname: %s\n   -Bildbreite: %d\n   -Anzahl der Kreise: %d\n.\n.\n.\n", name, width, circleCount);
-    } else {
-        printf("Fehlerhafte Eingabe, Programm wird beendet!\n\n");
-        break;
-    }
+        //set height to width to get quadratic image
+        height = width;
+        //Concatenate string with sprintf function to generate the filename
+        sprintf(filename, "%s_%d_%d.bmp", name, width, circleCount);
 
-    //set height to width to get quadratic image
-    height = width;
-    //Concatenate string with sprintf function to generate the filename
-    sprintf(filename, "%s_%d_%d.bmp", name, width, circleCount);
+        //execute functions to generate blanc image and draw circles with given parameters
+        char* image = genBackgroundImage(width, height, 222);
+        drawCircles(image, width, height, circleCount, 10000, 7);
+        saveBmpGray(filename, width, height, image);
 
-    //execute functions to generate blanc image and draw circles with given parameters
-    char* image = genBackgroundImage(width, height, 222);
-    drawCircles(image, width, height, circleCount, 10000, 7);
-    saveBmpGray(filename, width, height, image);
-
-    //free allocated storage to prevent overflow
-    free(image);
-    // end application successfully
-    printf("\n------Bild erfolgreich erzeugt------\n\n");
+        //free allocated storage to prevent overflow
+        free(image);
+        // end application successfully
+        printf("\n------Bild erfolgreich erzeugt------\n\n");
     }
     return 0;
 }
