ae128fc2314ee9c99b4d108e7a77e1789c22438e
johanneshi <johannes.hirschbeck@hm.edu>
2023-05-04 21:31:17 +0200

    completed task5 of ass2 and implemented "_gray.bmp" addition to filename

--- Praktikum/Versuch02/versuch02.c
+++ Praktikum/Versuch02/versuch02.c
@@ -6,7 +6,37 @@
 // main application
 int main(int argc, char* argv[])
 {
-    char filename[200] = "test/codeblocks1.bmp";
+    //cover wrong commandinput
+    if (argc != 2)
+    {
+        printf("Usage: %s <RGB_file>\n", argv[0]);
+        return 1;
+    }
+    //check file existance
+    char filename[200];
+    strcpy(filename, argv[1]);
+    if (existBmp(filename) == -1) return 2;
+
+    //create new filename which includes _gray
+    char new_filename[200];
+    strcpy(new_filename, filename);
+
+    //find . in filename to get pointer to that address
+    char* dot_ptr = strchr(new_filename,'.');
+
+    //overwrite everything after . in origin filename using dot_ptr
+    if (dot_ptr != NULL)
+    {
+        strcpy(dot_ptr, "_gray.bmp");
+    }
+
+    //in case filename does not have .bmp ending (highly unlikely because main fails before)
+    else
+    {
+        strcat(new_filename, "_gray.bmp");
+    }
+
+
 
     //get Image Properties
     uint32_t data_size = getBmpDataSize(filename), width = getBmpWidth(filename), height = getBmpHeight(filename);
@@ -23,7 +53,7 @@ int main(int argc, char* argv[])
     uint8_t *image = convertRgb2Gray(data, data_size/3);
 
     //safe converted image and freeup allocated space
-    saveBmpGray("Test2.bmp", width, height , image);
+    saveBmpGray(new_filename, width, height, image);
     free(image);
     return 0;
 }
